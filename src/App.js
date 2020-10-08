46c09870e771ae3d03e7d3cf18b07dc4dd231c15
ofdev <olivefdev@gmail.com>
2020-10-08 14:40:06 +0200

    move login in app and not in home only

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
-import { Router } from '@reach/router'
+import { Router, navigate } from '@reach/router'
 import { Home } from './Home'
 import { Admin } from './Admin'
 import { HomeAdmin } from './components/HomeAdmin'
 import { AdminUser } from './components/AdminUser'
 import { Raid } from './components/Raid'
+import Cookies from 'js-cookie'
+const api = 'http://localhost:5000/api/discord/login'
+const redirectRoot = 'http://localhost:3000'
+
+const redirect = encodeURIComponent('http://localhost:3000/')
+const targetBase = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=identify%20email%20guilds`
 
 const App = () => {
+	const [userInfos, setUserInfos] = useState({})
+
+	const login = async (cookieParsed, code) => {
+		if (cookieParsed) {
+			const response = await fetch(`${api}`, {
+				method: 'POST',
+				headers: {
+					'content-Type': 'application/json',
+				},
+				body: JSON.stringify(cookieParsed),
+			})
+			const json = await response.json()
+			setUserInfos(json)
+		} else if (!cookieParsed && code) {
+			const codeFormated = { code: window.location.search.split('=')[1] }
+			const response = await fetch(`${api}/token`, {
+				method: 'POST',
+				headers: {
+					'content-Type': 'application/json',
+				},
+				body: JSON.stringify(codeFormated),
+			})
+			const json = await response.json()
+			Cookies.set(
+				'aegis_discord',
+				{
+					token_type: json.token.token_type,
+					access_token: json.token.access_token,
+				},
+				{ expires: 7 }
+			)
+			navigate(`${redirectRoot}`)
+			setUserInfos(json.userInfos)
+		} else {
+			navigate(targetBase)
+		}
+	}
+
+	useEffect(() => {
+		const cookies = Cookies.get('aegis_discord')
+		const cookieParsed = cookies ? JSON.parse(cookies) : false
+		if (cookieParsed && cookieParsed.access_token && cookieParsed.token_type) {
+			login(cookieParsed)
+		} else if (window.location.search) {
+			login(null, window.location.search)
+		}
+	}, [])
+
 	return (
 		<div className='App'>
 			<Router>
-				<Home path='/' />
+				<Home path='/' userInfos={userInfos} login={login} />
 				<Admin path='/admin'>
 					<HomeAdmin path='/' />
 					<AdminUser path='user' />
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { Link, navigate } from '@reach/router'
-import Cookies from 'js-cookie'
+import React, { useState } from 'react'
+import { Link } from '@reach/router'
+
 import { Button } from 'antd'
 import { Carrousel } from './components/Carrousel'
 import { NavBar } from './components/NavBar'
@@ -8,62 +8,10 @@ import { DrawerCharacter } from './components/Drawer'
 
 import './Home.css'
 
-const api = 'http://localhost:5000/api/discord/login'
-const redirectRoot = 'http://localhost:3000'
-
-const redirect = encodeURIComponent('http://localhost:3000/')
-const targetBase = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=identify%20email%20guilds`
-
 export const Home = (props) => {
-	const [userInfos, setUserInfos] = useState({})
+	const { userInfos, login } = props
 	const [visible, setVisible] = useState(false)
 	const [character, setCharacter] = useState({})
-
-	const login = async (cookieParsed, code) => {
-		if (cookieParsed) {
-			const response = await fetch(`${api}`, {
-				method: 'POST',
-				headers: {
-					'content-Type': 'application/json',
-				},
-				body: JSON.stringify(cookieParsed),
-			})
-			const json = await response.json()
-			setUserInfos(json)
-		} else if (!cookieParsed && code) {
-			const codeFormated = { code: window.location.search.split('=')[1] }
-			const response = await fetch(`${api}/token`, {
-				method: 'POST',
-				headers: {
-					'content-Type': 'application/json',
-				},
-				body: JSON.stringify(codeFormated),
-			})
-			const json = await response.json()
-			Cookies.set(
-				'aegis_discord',
-				{
-					token_type: json.token.token_type,
-					access_token: json.token.access_token,
-				},
-				{ expires: 7 }
-			)
-			navigate(`${redirectRoot}`)
-			setUserInfos(json.userInfos)
-		} else {
-			navigate(targetBase)
-		}
-	}
-
-	useEffect(() => {
-		const cookies = Cookies.get('aegis_discord')
-		const cookieParsed = cookies ? JSON.parse(cookies) : false
-		if (cookieParsed && cookieParsed.access_token && cookieParsed.token_type) {
-			login(cookieParsed)
-		} else if (window.location.search) {
-			login(null, window.location.search)
-		}
-	}, [])
 
 	return (
 		<div>
